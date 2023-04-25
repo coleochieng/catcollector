7fbed958b92980110c9be6bc31a9e19320ce586d
coleochieng <nicole.ochieng98@gmail.com>
2023-04-24 23:08:38 -0500

    delete views.home

--- catcollector/main_app/views.py
+++ catcollector/main_app/views.py
@@ -2,9 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def home(request):
-    return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
-
 def about(request):
     return render(request, 'about.html')
 
