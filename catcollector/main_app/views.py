a96071a366cab2ccaebe74a5e5973020a35cfc2e
coleochieng <nicole.ochieng98@gmail.com>
2023-04-24 22:35:53 -0500

    add Cats class to views

--- catcollector/main_app/views.py
+++ catcollector/main_app/views.py
@@ -7,3 +7,20 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def cats_index(request):
+    return render(request, 'cats/index.html', { "cats": cats })
+
+class Cat:
+    def __init__(self, name, breed, description, age):
+        self.name = name
+        self.breed = breed
+        self.description = description
+        self.age = age
+
+cats = [
+  Cat('Lolo', 'tabby', 'foul little demon', 3),
+  Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
+  Cat('Raven', 'black tripod', '3 legged cat', 4)
+]
+
